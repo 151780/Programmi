f3131da60d9d7712325ff5ce82d6a892326ad101
robbymazzoni <52409624+robbymazzoni@users.noreply.github.com>
2024-05-25 11:56:30 +0200

    forecast.html

--- ServerFlask/main.py
+++ ServerFlask/main.py
@@ -303,7 +303,7 @@ def forecastGraph():
 
     ds=[]                                         # li passo alla pagina html per mostrare il grafico
     for i in range(len(ascisse)):
-        fTime = ascisse[i].strftime("%H:%M:%S")      
+        fTime = str(ascisse[i].strftime("%H:%M:%S"))
         ds.append([fTime,pioggiaReale[i],pioggiaPrevista[i]])
     print(ds)
     return json.dumps(ds),200
