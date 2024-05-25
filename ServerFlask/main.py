0e25a46b67db5998f8d6e2ddbc7683afe5d24837
robbymazzoni <52409624+robbymazzoni@users.noreply.github.com>
2024-05-25 15:27:41 +0200

    login updated

--- ServerFlask/main.py
+++ ServerFlask/main.py
@@ -443,7 +443,11 @@ def login():
         login_user(User(username))                                          # lo porto al menu generale
         return redirect('/static/menu.html')
     
-    return redirect('/static/login.html')               # altrimenti lo riporto a login
+    return render_template('login.html', error='invalid_credentials')               # altrimenti lo riporto a login con errore
+
+@app.route('/login')
+def show_login_form():
+    return render_template('login.html', error='')
 
 ### LOGOUT UTENTE
 @app.route('/logout', methods=["POST"])
