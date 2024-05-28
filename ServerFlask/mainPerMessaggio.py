f2360ff559df72d50e584c4614efea9d80cdd5d2
robbymazzoni <52409624+robbymazzoni@users.noreply.github.com>
2024-05-28 17:08:36 +0200

    .

--- ServerFlask/mainPerMessaggio.py
+++ ServerFlask/mainPerMessaggio.py
@@ -483,6 +483,8 @@ def signup():
             return render_template('signup.html')
         
         usersDB = updateUsersDB(username,password1,email,usersDB)           # altrimenti aggiorno DB
+        flash("User registered correctly", 'error')
+        return render_template("login.html")
         return redirect(url_for('login'))
     return render_template('signup.html')
 
